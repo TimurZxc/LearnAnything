74a69114adc2412e8f7e251a47385beafd0b5fc9
Alpysbayev Adilet Kanagatuly <96419013+alpysbayev@users.noreply.github.com>
2023-07-02 03:38:14 +0600

    youtube api

---
+++ api/youtube.py
@@ -0,0 +1,17 @@
+from googleapiclient.discovery import build
+
+api_key = "AIzaSyDr0JKn5WlHoUqVhqj0_21JYIXyZcbS1Ck"
+youtube = build("youtube", "v3", developerKey=api_key)
+
+
+def get_videos(amount: int):
+    search_query = "python tutorial"
+    response = youtube.search().list(
+        q=search_query,
+        part="id",
+        type="video",
+        maxResults=amount
+    ).execute()
+
+    video_ids = [item["id"]["videoId"] for item in response["items"]]
+    return video_ids
