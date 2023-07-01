2a31a287ce985915c5863367e11bab50f0b4ce79
Alpysbayev Adilet Kanagatuly <96419013+alpysbayev@users.noreply.github.com>
2023-07-02 03:39:26 +0600

    fixed yt api

--- api/youtube.py
+++ api/youtube.py
@@ -4,8 +4,7 @@ api_key = "AIzaSyDr0JKn5WlHoUqVhqj0_21JYIXyZcbS1Ck"
 youtube = build("youtube", "v3", developerKey=api_key)
 
 
-def get_videos(amount: int):
-    search_query = "python tutorial"
+def get_videos(search_query: str, amount: int):
     response = youtube.search().list(
         q=search_query,
         part="id",
